22f190bd21b07d3138073f28fee5c4c6c0cb5321
TimKap <timur.kap@yandex.ru>
2017-03-08 23:48:07 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,20 @@
 package ru.job4j;
 
+/**
+* Class Calculate для вычисления арифметических операций + - * / ^.
+* @author Timur Kapkaev (tim.kap@yandex.ru)
+* @since 08.03.2017
+* @version 1 
+*/
 public class Calculate {
-	public static void main (String[] args){
+	/**
+	* Вывод строки "Hello world"
+	* @author Timur Kapkaev
+	* @since 08.03.2017
+	* @param args  массив аргументов командной строки
+	* @return  возвращаемый параметр отсутствует
+	*/
+	public static void main (String[] args) {
 		System.out.println("Hello world");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Пакет простого калькулятора
+* @author Timur Kapkaev (tim.kap@yandex.ru)
+* @version $Id$
+* @since 08.03.2017
+*/
+package ru.job4j;

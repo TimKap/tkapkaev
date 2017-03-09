88ab9cc3c0ad37af4c10fe1ab0d60b2141c55db3
TimKap <timur.kap@yandex.ru>
2017-03-09 23:24:55 +0300

    task 4122

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Тест для класса Calculate
+* @author Timur KApkaev (timur.kap@yandex.ru)
+* @since 09.03.2017
+* @version $Id$
+*/
+
+public class CalculateTest {
+	/**
+	*Тест вывода на консоль "Hello World"
+	*/
+	@Test
+	public void whenAddOneToOneThenTwo() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(out.toString(), is("Hello World\r\n"));
+		}
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+*Пакет тестов для методов класса Calculate
+* @author Timur Kapakaev (timur.kap@yandex.ru)
+* @version $Id$
+* @since 09.03.2017
+*/
+package ru.job4j;

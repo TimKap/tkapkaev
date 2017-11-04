ddb8a65bd33f60f31df93835b03a2e573d17e98c
TimKap <timur.kap@yandex.ru>
2017-11-04 17:10:44 +0300

    task 1. Создать web проект [#2514]

---
+++ chapter_007/src/main/java/ru/job4j/firstservlets/EchoServlet.java
@@ -0,0 +1,39 @@
+package ru.job4j.firstservlets;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * Class EchoServlet описывает сервлет, возвращающий эхо-овет.
+ * @author Timur Kpakaev (timur.kap@yandex.ru)
+ * @version $ID$
+ * @since 03.07.2017
+ * */
+public class EchoServlet extends HttpServlet {
+    /** логгер. */
+    private static final Logger LOGGER = LogManager.getLogger(EchoServlet.class);
+
+    /**
+     * Обработка пользовательского запроса.
+     * @param req - запрос
+     * @param resp - ответ
+     * @throws ServletException - исключение при обработке запроса
+     * @throws IOException - исключение ввода/вывода
+     * */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+       try (PrintWriter writer = new PrintWriter(resp.getOutputStream())) {
+           writer.append("Hello World.");
+           writer.flush();
+       }
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/firstservlets/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Пакет содержит решения к задачам на сервлеты.
+ * @author Timur Kapkaev (timur.kap@yandex.ru)
+ * @version $ID$
+ * @since 03.11.2017
+ * */
+package ru.job4j.firstservlets;
